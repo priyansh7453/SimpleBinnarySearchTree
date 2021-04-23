88c83762652f4de543017a6b40b1a8e98339f2f9
Priyansh <priyansh.kushwaha_cs19@gla.ac.in>
2021-04-23 16:34:40 +0530

    Insert Node

---
+++ src/Main/MainClass.java
@@ -0,0 +1,19 @@
+package Main;
+import implementation.mybinary;
+
+public class MainClass {
+    public static void main(String[] args) {
+        mybinary obj = new mybinary();
+        obj.insert(55);
+        obj.insert(23);
+        obj.insert(11);
+        obj.insert(77);
+        obj.insert(00);
+        obj.insert(67);
+        System.out.println("input data" + obj);
+        obj.isEmpty();
+        obj.maxHeight(obj.);
+        obj.inOrder(obj.r);
+        obj.search(23);
+    }
+}
